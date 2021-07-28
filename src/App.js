e281ab7cadb0eae7c9d4967cc98a5fa2cfde60e4
Andrew Starlike <andrewstarlike@gmail.com>
2021-07-29 01:59:44 +0300

    form input validation example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import SimpleInput from './components/SimpleInput';
+import useForm from "./hooks/useForm";
+import { signupForm } from "./utils/formConfig";
+import classes from "./App.module.css";
 
 function App() {
+  const { renderFormInputs, isFormValid } = useForm(signupForm);
+
   return (
-    <div className="app">
-      <SimpleInput />
-    </div>
+    <form className={classes.signupForm}>
+      <h1>Sign Up</h1>
+      {renderFormInputs()}
+      <button
+        type="submit"
+        disabled={!isFormValid()}
+        className={classes.button}
+      >
+        Submit
+      </button>
+    </form>
   );
 }
 
